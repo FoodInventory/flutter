dbf54ec16487d2d956aa7abd642deed78831c274
bastianfbr <badmax202@gmail.com>
2023-07-29 23:32:21 +0200

    feat: :lipstick: update HomePage with NavigationBar and AppBar

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -6,32 +6,58 @@ class MyHomePage extends StatefulWidget {
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyHomePage> createState() => MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class MyHomePageState extends State<MyHomePage> {
+  int currentPageIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: const Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'Nothing to do',
+        appBar: AppBar(
+          title: Text(
+            widget.title,
+            style: const TextStyle(fontWeight: FontWeight.bold),
+          ),
+          elevation: 20,
+          centerTitle: mounted,
+        ),
+        bottomNavigationBar: NavigationBar(
+          onDestinationSelected: (int index) {
+            setState(() {
+              currentPageIndex = index;
+            });
+          },
+          selectedIndex: currentPageIndex,
+          destinations: const <Widget>[
+            NavigationDestination(
+              selectedIcon: Icon(Icons.home_rounded),
+              icon: Icon(Icons.home_outlined),
+              label: 'Home',
             ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.list_rounded),
+              icon: Icon(Icons.list_outlined),
+              label: 'List',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.add_rounded),
+              icon: Icon(Icons.add_outlined),
+              label: 'Add',
+            ),
+            NavigationDestination(
+              selectedIcon: Icon(Icons.settings_rounded),
+              icon: Icon(Icons.settings_outlined),
+              label: 'Settings',
+            )
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Nothing',
-        child: const Icon(Icons.not_interested),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        body: <Widget>[
+          Container(),
+          Container(),
+          Container(),
+          Container()
+        ][currentPageIndex]);
   }
 }
