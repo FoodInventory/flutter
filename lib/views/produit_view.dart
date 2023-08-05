498e841ee945d2b6abbbfb24b5d79691d98c1b2a
bastianfbr <badmax202@gmail.com>
2023-08-05 16:43:05 +0200

    feat: :lipstick: update ProduitView

--- lib/views/produit_view.dart
+++ lib/views/produit_view.dart
@@ -27,13 +27,54 @@ class _ProduitViewState extends State<ProduitView> {
 
   Card createSampleProduit() {
     return Card(
-      child: ListTile(
-        leading: Image.network(
-            "https://images.openfoodfacts.org/images/products/327/408/000/5003/front_fr.950.full.jpg"),
-        title: const Text('Eau de source - Cristaline'),
-        subtitle: const Text('Quantite: 1.5L\nNombre: 6'),
-        trailing: const Icon(Icons.more_vert),
-        isThreeLine: true,
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+          contentPadding: const EdgeInsets.all(0.0),
+          leading: ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.network(
+              "https://images.openfoodfacts.org/images/products/327/408/000/5003/front_fr.950.full.jpg",
+              height: 80,
+              width: 50,
+              fit: BoxFit.cover,
+            ),
+          ),
+          title: const Text(
+            'Eau de source - Cristaline',
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          subtitle: const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(height: 4),
+              Text(
+                'Quantité: 1.5L',
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+              Text(
+                'Nombre: 6',
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+            ],
+          ),
+          trailing: IconButton(
+            icon: const Icon(Icons.more_vert),
+            onPressed: () {
+              // Edit menu
+            },
+          ),
+        ),
       ),
     );
   }
