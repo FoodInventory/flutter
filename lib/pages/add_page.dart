dd8cae9eae458b63c3b8f43cb1cbf672bb141882
bastianfbr <badmax202@gmail.com>
2023-08-05 16:42:26 +0200

    feat: :lipstick: add AddPage UI

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -10,6 +10,86 @@ class AddPage extends StatefulWidget {
 class _AddPageState extends State<AddPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Dialog(
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Catégorie du produit',
+                  icon: Icon(Icons.category),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Nom du produit',
+                  icon: Icon(Icons.shopping_bag),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Marque du produit',
+                  icon: Icon(Icons.branding_watermark),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Quantité du produit',
+                  icon: Icon(Icons.format_list_numbered),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Unité du produit',
+                  icon: Icon(Icons.storage),
+                ),
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                decoration: const InputDecoration(
+                  labelText: 'Nombre de ce produit',
+                  icon: Icon(Icons.filter_1),
+                ),
+              ),
+              const SizedBox(height: 24),
+              Row(
+                children: [
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        // logique du scanner de code-barres
+                      },
+                      icon: const Icon(Icons.qr_code_scanner),
+                      label: const Text('Scanner'),
+                    ),
+                  ),
+                  const SizedBox(width: 16),
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () {
+                        // logique du POST de l'API Strapi
+                      },
+                      icon: const Icon(Icons.add),
+                      label: const Text('Ajouter'),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
