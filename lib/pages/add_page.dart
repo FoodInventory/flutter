6d2dcca4aba28884b14feadf6e316f313f6af090
bastianfbr <badmax202@gmail.com>
2023-08-05 22:10:41 +0200

    feat: :sparkles: add barcode scanner and POST request to Strapi API

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
+import 'package:openfoodfacts/openfoodfacts.dart';
+import 'package:http/http.dart' as http;
 
 class AddPage extends StatefulWidget {
   const AddPage({super.key});
@@ -8,6 +13,66 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
+  final TextEditingController categorieController = TextEditingController();
+  final TextEditingController nomController = TextEditingController();
+  final TextEditingController marqueController = TextEditingController();
+  final TextEditingController quantiteController = TextEditingController();
+  final TextEditingController uniteController = TextEditingController();
+  final TextEditingController nombreController = TextEditingController();
+
+  String scannedCode = '';
+  String productImage = '';
+
+  void _onScanButtonPressed() async {
+    String code = await FlutterBarcodeScanner.scanBarcode(
+      '#ff6666',
+      'Cancel',
+      true,
+      ScanMode.BARCODE,
+    );
+
+    ProductQueryConfiguration config = ProductQueryConfiguration(
+      code,
+      version: ProductQueryVersion.v3,
+    );
+
+    ProductResultV3 product = await OpenFoodAPIClient.getProductV3(config);
+
+    setState(() {
+      productImage = product.product!.imageFrontUrl!;
+      scannedCode = code;
+      categorieController.text = scannedCode;
+      nomController.text = product.product!.productName!;
+      marqueController.text = product.product!.brands!;
+      quantiteController.text = product.product!.quantity!;
+    });
+  }
+
+  _onAddButtonPressed() {
+    var client = http.Client();
+    var uri = Uri.parse('https://foodapi.bastianfabre.fr/api/produits');
+
+    Map<String, dynamic> data = {
+      'barcode': scannedCode,
+      'categorie': categorieController.text,
+      'nom': nomController.text,
+      'marque': marqueController.text,
+      'quantite': quantiteController.text,
+      'unite': uniteController.text,
+      'nombre': nombreController.text,
+      'image': productImage,
+    };
+
+    String jsonString = jsonEncode({'data': data});
+
+    return client.post(uri, body: jsonString, headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization':
+          'Bearer ae9f7fdc0d69f0f4a437feacf4bbf8faea4587b7ce7eac3ff43e25635942b10779de3ff006885236fb7000117b9935a84354fb84541d6fde61d2bc746fd442a484d4417a706ca1797e7fcc4d03506a520146dd089f5a6ae069787c0847bc2a11f7d27b88f74761e240112873c5729be1b03def7211c3f7aa0d6237795a4e7bcb',
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Dialog(
@@ -22,6 +87,7 @@ class _AddPageState extends State<AddPage> {
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: [
               TextFormField(
+                controller: categorieController,
                 decoration: const InputDecoration(
                   labelText: 'Catégorie du produit',
                   icon: Icon(Icons.category),
@@ -29,6 +95,7 @@ class _AddPageState extends State<AddPage> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: nomController,
                 decoration: const InputDecoration(
                   labelText: 'Nom du produit',
                   icon: Icon(Icons.shopping_bag),
@@ -36,6 +103,7 @@ class _AddPageState extends State<AddPage> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: marqueController,
                 decoration: const InputDecoration(
                   labelText: 'Marque du produit',
                   icon: Icon(Icons.branding_watermark),
@@ -43,6 +111,7 @@ class _AddPageState extends State<AddPage> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: quantiteController,
                 decoration: const InputDecoration(
                   labelText: 'Quantité du produit',
                   icon: Icon(Icons.format_list_numbered),
@@ -50,6 +119,7 @@ class _AddPageState extends State<AddPage> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: uniteController,
                 decoration: const InputDecoration(
                   labelText: 'Unité du produit',
                   icon: Icon(Icons.storage),
@@ -57,6 +127,7 @@ class _AddPageState extends State<AddPage> {
               ),
               const SizedBox(height: 16),
               TextFormField(
+                controller: nombreController,
                 decoration: const InputDecoration(
                   labelText: 'Nombre de ce produit',
                   icon: Icon(Icons.filter_1),
@@ -68,7 +139,7 @@ class _AddPageState extends State<AddPage> {
                   Expanded(
                     child: ElevatedButton.icon(
                       onPressed: () {
-                        // logique du scanner de code-barres
+                        _onScanButtonPressed();
                       },
                       icon: const Icon(Icons.qr_code_scanner),
                       label: const Text('Scanner'),
@@ -77,8 +148,8 @@ class _AddPageState extends State<AddPage> {
                   const SizedBox(width: 16),
                   Expanded(
                     child: ElevatedButton.icon(
-                      onPressed: () {
-                        // logique du POST de l'API Strapi
+                      onPressed: () async {
+                        await _onAddButtonPressed();
                       },
                       icon: const Icon(Icons.add),
                       label: const Text('Ajouter'),
