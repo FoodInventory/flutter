ebff2ce8a67fb3e437b8235bf84344c581ef8e12
bastianfbr <badmaxyt@gmail.com>
2023-08-07 16:59:56 +0200

    refactor: :recycle: create ProduitCard component

---
+++ lib/components/produit_card.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class ProduitCard extends StatefulWidget {
+  final String nom;
+  final String image;
+  final double quantite;
+  final String unite;
+  final int nombre;
+
+  const ProduitCard(
+      {super.key,
+      required this.nom,
+      required this.image,
+      required this.quantite,
+      required this.unite,
+      required this.nombre});
+
+  @override
+  State<ProduitCard> createState() => _ProduitCardState();
+}
+
+class _ProduitCardState extends State<ProduitCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: ListTile(
+            contentPadding: const EdgeInsets.all(0.0),
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.network(
+                widget.image,
+                height: 80,
+                width: 50,
+                fit: BoxFit.cover,
+              ),
+            ),
+            title: Text(
+              widget.nom,
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            subtitle: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 4),
+                Text(
+                  'Quantité: ${widget.quantite} ${widget.unite}',
+                  style: const TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                Text(
+                  'Nombre: ${widget.nombre}',
+                  style: const TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {
+                // Edit menu
+              },
+            )),
+      ),
+    );
+  }
+}
--- lib/views/produit_view.dart
+++ lib/views/produit_view.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:food_inventory/components/produit_card.dart';
 import 'package:food_inventory/models/produit.dart';
 import 'package:http/http.dart' as http;
 
@@ -48,55 +49,12 @@ class _ProduitViewState extends State<ProduitView> {
           return ListView.builder(
             itemCount: snapshot.data!.length,
             itemBuilder: (context, index) {
-              return Card(
-                elevation: 4.0,
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: ListTile(
-                      contentPadding: const EdgeInsets.all(0.0),
-                      leading: ClipRRect(
-                        borderRadius: BorderRadius.circular(8.0),
-                        child: Image.network(
-                          snapshot.data![index].image!,
-                          height: 80,
-                          width: 50,
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                      title: Text(
-                        snapshot.data![index].nom!,
-                        style: const TextStyle(
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      subtitle: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          const SizedBox(height: 4),
-                          Text(
-                            'Quantité: ${snapshot.data![index].quantite} ${snapshot.data![index].unite}',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                          Text(
-                            'Nombre: ${snapshot.data![index].nombre}',
-                            style: const TextStyle(
-                              fontSize: 14,
-                            ),
-                          ),
-                        ],
-                      ),
-                      trailing: IconButton(
-                        icon: const Icon(Icons.more_vert),
-                        onPressed: () {
-                          // Edit menu
-                        },
-                      )),
-                ),
+              return ProduitCard(
+                nom: snapshot.data![index].nom!,
+                image: snapshot.data![index].image!,
+                quantite: snapshot.data![index].quantite,
+                unite: snapshot.data![index].unite,
+                nombre: snapshot.data![index].nombre,
               );
             },
           );
