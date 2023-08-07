6eb1f0447d42c9d82440bde586a2424b992900bf
bastianfbr <badmaxyt@gmail.com>
2023-08-07 15:58:33 +0200

    feat: :sparkles: add getProducts

--- lib/views/produit_view.dart
+++ lib/views/produit_view.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:food_inventory/models/produit.dart';
+import 'package:http/http.dart' as http;
 
 class ProduitView extends StatefulWidget {
   const ProduitView({super.key});
@@ -8,6 +12,35 @@ class ProduitView extends StatefulWidget {
 }
 
 class _ProduitViewState extends State<ProduitView> {
+  Future<List<Produit>> _getProducts() async {
+    var client = http.Client();
+    var uri = Uri.parse('https://foodapi.bastianfabre.fr/api/produits');
+    await client.get(uri, headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ${dotenv.env['API_TOKEN']!}',
+    }).then(
+      (response) {
+        if (response.statusCode == 200) {
+          var produitList = <Produit>[];
+          var produits = jsonDecode(response.body)['data'];
+          for (var produit in produits) {
+            var produitParse = produit['attributes'];
+            produitParse.remove('updatedAt');
+            produitParse.remove('createdAt');
+            produitParse.remove('deletedAt');
+            produitParse.remove('locale');
+
+            produitList.add(Produit.fromJson(produitParse));
+          }
+          return produitList;
+        }
+        return <Produit>[];
+      },
+    );
+    return <Produit>[];
+  }
+
   @override
   Widget build(BuildContext context) {
     // return a list of created sample products;
@@ -34,47 +67,46 @@ class _ProduitViewState extends State<ProduitView> {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: ListTile(
-          contentPadding: const EdgeInsets.all(0.0),
-          leading: ClipRRect(
-            borderRadius: BorderRadius.circular(8.0),
-            child: Image.network(
-              "https://images.openfoodfacts.org/images/products/327/408/000/5003/front_fr.950.full.jpg",
-              height: 80,
-              width: 50,
-              fit: BoxFit.cover,
-            ),
-          ),
-          title: const Text(
-            'Eau de source - Cristaline',
-            style: TextStyle(
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          subtitle: const Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              SizedBox(height: 4),
-              Text(
-                'Quantité: 1.5L',
-                style: TextStyle(
-                  fontSize: 14,
-                ),
+            contentPadding: const EdgeInsets.all(0.0),
+            leading: ClipRRect(
+              borderRadius: BorderRadius.circular(8.0),
+              child: Image.network(
+                "https://images.openfoodfacts.org/images/products/327/408/000/5003/front_fr.950.full.jpg",
+                height: 80,
+                width: 50,
+                fit: BoxFit.cover,
               ),
-              Text(
-                'Nombre: 6',
-                style: TextStyle(
-                  fontSize: 14,
-                ),
+            ),
+            title: const Text(
+              'Eau de source - Cristaline',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
               ),
-            ],
-          ),
-          trailing: IconButton(
-            icon: const Icon(Icons.more_vert),
-            onPressed: () {
-              // Edit menu
-            },
-          ),
-        ),
+            ),
+            subtitle: const Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(height: 4),
+                Text(
+                  'Quantité: 1.5L',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+                Text(
+                  'Nombre: 6',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+            trailing: IconButton(
+              icon: const Icon(Icons.more_vert),
+              onPressed: () {
+                // Edit menu
+              },
+            )),
       ),
     );
   }
