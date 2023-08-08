9a7578963405efd61bc7a5e7caf03031c650b91e
bastianfbr <badmaxyt@gmail.com>
2023-08-09 00:10:23 +0200

    feat: :lipstick: update theme with "Monet" like colorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:food_inventory/pages/app_menu.dart';
+import 'package:dynamic_color/dynamic_color.dart';
 
 Future<void> main() async {
   await dotenv.load();
@@ -10,21 +11,37 @@ Future<void> main() async {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static final _defaultLightColorScheme = ColorScheme.fromSwatch(
+    primarySwatch: Colors.blue,
+    primaryColorDark: Colors.blue[700],
+    accentColor: Colors.blueAccent[100],
+    backgroundColor: Colors.blue[50],
+    brightness: Brightness.light,
+  );
+
+  static final _defaultDarkColorScheme = ColorScheme.fromSwatch(
+    primarySwatch: Colors.blue,
+    primaryColorDark: Colors.blue[700],
+    accentColor: Colors.blueAccent[100],
+    backgroundColor: Colors.blue[50],
+    brightness: Brightness.dark,
+  );
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Food Inventory',
-      theme: ThemeData(
-        colorScheme: ColorScheme?.fromSwatch(
-          primarySwatch: Colors.blue,
-          primaryColorDark: Colors.blue[700],
-          accentColor: Colors.blueAccent[100],
-          backgroundColor: Colors.blue[50],
-          brightness: Brightness.light,
+    return DynamicColorBuilder(builder: (lightColorScheme, darkColorScheme) {
+      return MaterialApp(
+        title: 'Food Inventory',
+        theme: ThemeData(
+          colorScheme: lightColorScheme ?? _defaultLightColorScheme,
+          useMaterial3: true,
         ),
-        useMaterial3: true,
-      ),
-      home: const AppMenu(title: 'Food Inventory'),
-    );
+        darkTheme: ThemeData(
+          colorScheme: darkColorScheme ?? _defaultDarkColorScheme,
+          useMaterial3: true,
+        ),
+        home: const AppMenu(title: 'Food Inventory'),
+      );
+    });
   }
 }
