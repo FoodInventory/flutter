6f2e783014e25fb2b0615aa661e6151bb3604a56
bastianfbr <badmaxyt@gmail.com>
2023-08-13 17:44:04 +0200

    feat: :lipstick: add sample List UI

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -10,6 +10,124 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: const EdgeInsets.only(top: 100),
+      child: const Column(
+        children: [
+          Card(
+            child: SizedBox(
+              height: 100,
+              child: Center(
+                child: ListTile(
+                  title: Text(
+                    'Liste de la cave',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  leading: Icon(
+                    Icons.home_rounded,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Card(
+            child: SizedBox(
+              height: 100,
+              child: Center(
+                child: ListTile(
+                  title: Text(
+                    'Liste des courses',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  leading: Icon(
+                    Icons.shopping_bag_rounded,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Card(
+            child: SizedBox(
+              height: 100,
+              child: Center(
+                child: ListTile(
+                  title: Text(
+                    'Liste de trucs',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  leading: Icon(
+                    Icons.onetwothree_sharp,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Card(
+            child: SizedBox(
+              height: 100,
+              child: Center(
+                child: ListTile(
+                  title: Text(
+                    'Liste de machins',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  leading: Icon(
+                    Icons.table_restaurant_rounded,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Card(
+            child: SizedBox(
+              height: 100,
+              child: Center(
+                child: ListTile(
+                  title: Text(
+                    'Liste de bidules',
+                    style: TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  leading: Icon(
+                    Icons.weekend_sharp,
+                    size: 40,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
